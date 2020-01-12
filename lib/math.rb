ff83a71df011e9a3d71a0dad0193b4451badb895
TShipley <taneymail@gmail.com>
2020-01-12 14:03:50 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,19 +1,19 @@
 def addition(num1, num2)
-
+ num1 + num2
 end
-
+ 
 def subtraction(num1, num2)
-
+ num1 - num2
 end
-
+ 
 def division(num1, num2)
-
+ num1 * num2
 end
-
+ 
 def multiplication(num1, num2)
-
+ num1 / num2
 end
-
+ num1 modulo num2
 def modulo(num1, num2)
 
 end
